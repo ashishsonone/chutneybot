2903714152e52835e228bbd9e950fee2ebcae032
ashishsonone <ashson07@gmail.com>
2016-08-08 21:40:05 +0530

    tried to edit according to jslint warning messages

--- app/api/nodes/work.js
+++ app/api/nodes/work.js
@@ -1,20 +1,22 @@
+'use strict';
+
 var workDb = require('../db/work');
 
-function getCompanyWorkReply(companyEntities){
+function getCompanyWorkReply(companyEntities) {
   delete companyEntities.map["general"]; //remove general key
       
   var companies = Object.keys(companyEntities.map); //get other value of companies
+  var company = companies[0];
   console.log("work.company node : %j", companies);
 
-  var company = companies[0];
 
   var reply = [];
-  if(company){
+  if (company) {
     company = company.toLowerCase();
     console.log("company work %j", workDb.companies[company]);
   }
 
-  if(!company){
+  if (!company) {
     reply.push({
       type : "text",
       value : "Here are our general best works"
@@ -22,7 +24,7 @@ function getCompanyWorkReply(companyEntities){
 
     reply = reply.concat(workDb.work["general"]);
   }
-  else if(company && workDb.companies[company]){
+  else if (company && workDb.companies[company]) {
     reply.push({
       type : "text",
       value : "Here is our work for client " + company
@@ -35,7 +37,7 @@ function getCompanyWorkReply(companyEntities){
       ]
     }); //push modifies original array and returns only NUMBER
   }
-  else{
+  else {
     reply.push({
       type : "text",
       value : "We haven't worked for " + company + " but look at our other works"
@@ -50,12 +52,12 @@ function getCompanyWorkReply(companyEntities){
 var tree = {
   "work.root" : {
     id : "work.root",
-    condition : function(session){
+    condition : function (session) {
       var entities = session.state.entities;
       return !(entities["company"]); //no company entity
     },
     
-    reply : function(session){
+    reply : function (session) {
       var reply = [{
         type : "text",
         value : "Here are our general best work"
@@ -65,7 +67,7 @@ var tree = {
       return {
         reply : reply,
         suggestions : ["fmcg work", "how to contact"]
-      }
+      };
     },
     
     child : null,
@@ -75,18 +77,18 @@ var tree = {
   
   "work.company" : {
     id : "work.company",
-    condition : function(session){
+    condition : function (session) {
       return session.state.entities["company"];
     },
     
-    reply : function(session){
+    reply : function (session) {
       var companyEntities = session.state.entities["company"];
       
       var reply = getCompanyWorkReply(companyEntities);
       return {
         reply : reply,
         suggestions : ["how to contact"]
-      }
+      };
     },
     
     child : null,
