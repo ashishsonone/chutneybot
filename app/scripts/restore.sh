3c9904076b48cb9ddde875da82afe734eee51a7d
Ashish Sonone <ashson07@gmail.com>
2016-10-12 14:48:53 +0530

    updated scripts

--- app/scripts/restore.sh
+++ app/scripts/restore.sh
@@ -6,9 +6,11 @@
 cd ~/Downloads
 folder=$1
 echo $folder
-mongo chutney --eval "db.clients.drop(); db.people.drop(); db.work.drop(); db.awards.drop(); db.contacts.drop()"
+#mongo chutney --eval "db.clients.drop(); db.people.drop(); db.work.drop(); db.awards.drop(); db.contacts.drop();"
 mongorestore --collection clients --db chutney $folder/chutney/clients.bson
 mongorestore --collection work --db chutney $folder/chutney/work.bson
 mongorestore --collection people --db chutney $folder/chutney/people.bson
 mongorestore --collection awards --db chutney $folder/chutney/awards.bson
 mongorestore --collection contacts --db chutney $folder/chutney/contacts.bson
+mongorestore --collection responses --db chutney $folder/chutney/responses.bson
+mongorestore --collection cases --db chutney $folder/chutney/cases.bson
